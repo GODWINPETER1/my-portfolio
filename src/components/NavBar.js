638803046675601237fc07cf1f3c065d54e268d4
Godwin <petergodwin437@icloud.com>
2025-02-26 15:18:39 +0300

    change the header color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Text href="/">
-             <h1> Godwin </h1>
+             <h1 className="header"> Godwin </h1>
           </Navbar.Text>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
